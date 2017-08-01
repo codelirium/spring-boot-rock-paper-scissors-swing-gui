6dfa3da70ec29bed719d1a8531baf8dd713940d4
Stavros Lekkas <stavros.lekkas@gmail.com>
2017-08-01 05:04:52 +0300

    refactor

--- src/main/java/io/codelirium/game/gui/GUILayout.java
+++ src/main/java/io/codelirium/game/gui/GUILayout.java
@@ -19,8 +19,6 @@ import java.util.List;
 
 import static io.codelirium.game.core.Mode.COMPUTER_VS_COMPUTER;
 import static io.codelirium.game.core.Mode.HUMAN_VS_COMPUTER;
-import static io.codelirium.game.core.model.Result.LOSE;
-import static io.codelirium.game.core.model.Result.WIN;
 import static java.util.Objects.nonNull;
 
 
@@ -41,21 +39,7 @@ public class GUILayout extends JFrame {
 
 		initSelectionAndStatusPanels();
 
-		JButton humanVSComputerButton = new JButton("Human VS Computer");
-		JButton computerVSComputerButton = new JButton("Computer VS Computer");
-		JButton quitButton = new JButton("Quit");
-
-		JPanel modePanel = new JPanel();
-
-		modePanel.add(humanVSComputerButton);
-		modePanel.add(computerVSComputerButton);
-		modePanel.add(quitButton);
-
-		getContentPane().add(modePanel, BorderLayout.PAGE_END);
-
-		humanVSComputerButton.addActionListener((ActionEvent event) -> humanVSComputerButtonAction());
-		computerVSComputerButton.addActionListener((ActionEvent event) -> computerVSComputerButtonAction());
-		quitButton.addActionListener((ActionEvent event) -> quitButtonAction());
+		initModePanel();
 	}
 
 	private void initSelectionAndStatusPanels() {
@@ -79,6 +63,25 @@ public class GUILayout extends JFrame {
 
 		statusPanel.setVisible(false);
 		getContentPane().add(statusPanel, BorderLayout.CENTER);
+	}
+
+	private void initModePanel() {
+
+		JButton humanVSComputerButton = new JButton("Human VS Computer");
+		JButton computerVSComputerButton = new JButton("Computer VS Computer");
+		JButton quitButton = new JButton("Quit");
+
+		JPanel modePanel = new JPanel();
+
+		modePanel.add(humanVSComputerButton);
+		modePanel.add(computerVSComputerButton);
+		modePanel.add(quitButton);
+
+		getContentPane().add(modePanel, BorderLayout.PAGE_END);
+
+		humanVSComputerButton.addActionListener((ActionEvent event) -> humanVSComputerButtonAction());
+		computerVSComputerButton.addActionListener((ActionEvent event) -> computerVSComputerButtonAction());
+		quitButton.addActionListener((ActionEvent event) -> quitButtonAction());
 	}
 
 	private void humanVSComputerButtonAction() {
@@ -132,24 +135,25 @@ public class GUILayout extends JFrame {
 
 		List<Choice> choices = new Game(mode).play(optionChoice);
 
+		String padding = "\t\t\t\t\t";
 		String playerOneString = mode.equals(COMPUTER_VS_COMPUTER) ? "Computer 1" : "Human 1";
-		String playerOneStringPadded = String.format("%s: %s\t\t\t\t\t", playerOneString, choices.get(0).getName());
-		String playerTwoStringPadded = String.format("\t\t\t\t\tComputer 2: %s", choices.get(1).getName());
+		String playerOneStringPadded = String.format("%s: %s%s", playerOneString, choices.get(0).getName(), padding);
+		String playerTwoStringPadded = String.format("%sComputer 2: %s", padding, choices.get(1).getName());
 
 		String statusString;
 
-		if (choices.get(0).gameOnWith(choices.get(1)).equals(WIN)) {
+		switch (choices.get(0).gameOnWith(choices.get(1))) {
 
-			statusString = String.format("%s\t\t\t\t[\t%s WINS!\t]\t\t\t%s", playerOneStringPadded, playerOneString, playerTwoStringPadded);
+			case WIN:
+				statusString = String.format("%s%s[ %s WINS! ]%s%s", playerOneStringPadded, padding, playerOneString, padding, playerTwoStringPadded);
+				break;
 
-		} else if (choices.get(0).gameOnWith(choices.get(1)).equals(LOSE)) {
+			case LOSE:
+				statusString = String.format("%s%s[ Computer 2 WINS! ]%s%s", playerOneStringPadded, padding, padding, playerTwoStringPadded);
+				break;
 
-			statusString = String.format("%s\t\t\t\t[\tComputer 2 WINS!\t]\t\t\t%s", playerOneStringPadded, playerTwoStringPadded);
-
-		} else {
-
-			statusString = String.format("%s\t\t\t[\tThe game is a TIE!\t]\t\t\t%s", playerOneStringPadded, playerTwoStringPadded);
-
+			default:
+				statusString = String.format("%s%s[ The game is a TIE! ]%s%s", playerOneStringPadded, padding, padding, playerTwoStringPadded);
 		}
 
 		statusPanel.add(new JLabel(statusString, SwingConstants.CENTER), BorderLayout.CENTER);
